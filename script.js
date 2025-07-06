d423220a4488fc235f142ffcdf424f64d7b7496c
hectorhdzreyes <hectorhdzreyes05@gmail.com>
2025-07-05 18:19:24 -0700

    preloader and errors

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+// Preloader function
+function loaderSpinner() {
+    $(window).load(function() {
+        var loader = $('.loader');
+        var wHeight = $(window).height();
+        var wWidth = $(window).width();
+        var i = 0;
+        
+        /*Center loader on half screen */
+        loader.css({
+            top: wHeight / 2 - 2.5,
+            left: wWidth / 2 - 200
+        })
+              
+        do{
+            loader.animate({
+                width: i
+            },10)
+            i+=3;
+        } while(i <= 400)
+        if(i === 402){
+            loader.animate({
+                left: 0,
+                width: '100%'
+            })
+            loader.animate({
+                top: '0',
+                height: '100vh'
+            })
+        }
+              
+        /* This line hide loader and show content */
+        setTimeout(function(){
+            $('.content').fadeIn("slow");
+            (loader).fadeOut("fast");
+            document.body.classList.add('loaded');
+            /*Set time in milisec */
+        },3500);
+    });
+}
+
+loaderSpinner();
+
 // Future JavaScript code will go here 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -217,7 +260,8 @@ if (mainTitleSection) {
 
 
   // Optional: Trigger animation when the services section is in view
-  // ... existing code ...
+}
 
+// ... existing code 
 
-// Removed old ScrollMagic related code that was here previously. 
+}); // Close DOMContentLoaded event listener
